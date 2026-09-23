637fd30a52b41ec995e88f6b08e8b8bff75e5f31
AngyAlcantara <alcantaraamorales@gmail.com>
2026-09-23 11:36:16 -0600

    Se agregan reglas para no permitir numeros negativos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,10 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
-  numero: number = 0;
 
-  constructor() {}
+  // Tu variable empieza en 0
+  contador: number = 0;
+
+  // Función para que el número avance (+1)
+  incrementar() {
+    this.contador++;
+  }
+
+  // Función para restar (-1) SIN permitir negativos
+  decrementar() {
+    // Solo resta si el número es mayor a 0
+    if (this.contador > 0) {
+      this.contador--;
+    }
+  }
+
+  // Función para volver a empezar en 0
+  reiniciar() {
+    this.contador = 0;
+  }
+
 }
